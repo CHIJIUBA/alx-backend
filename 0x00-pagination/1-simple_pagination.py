b0e768ddc1d0acc897f6c6e1485d1dc20bdb3cce
Chijiuba Victory <chijiubaonyedikachukwu@gmail.com>
2023-02-21 03:29:19 +0100

    go

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
+'''
+    Simple pagination.
+'''
 import csv
 import math
-from typing import List, Tuple
+from typing import List
 
 
 def index_range(page, page_size):
-    """Returns the start and end index pagination"""
+    '''
+        Returns the range of indexes for a given page.
+    '''
     start = (page - 1) * page_size
     end = page * page_size
     return start, end
@@ -19,7 +24,7 @@ class Server:
     def __init__(self):
         self.__dataset = None
 
-    def dataset(self) -> List[List]:
+    def dataset(self) -> List[List]:  # sourcery skip: identity-comprehension
         """Cached dataset
         """
         if self.__dataset is None:
@@ -31,12 +36,15 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """Paginated dataset
-        """
+        '''
+            Returns a page of data.
+        '''
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
         self.dataset()
+
         if self.dataset() is None:
             return []
-        my_tuple = index_range(page, page_size)
-        return self.dataset()[my_tuple[0]:my_tuple[1]]
+
+        indexRange = index_range(page, page_size)
+        return self.dataset()[indexRange[0]:indexRange[1]]
